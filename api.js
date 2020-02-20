621bceb1e93cb7b657c0fd109036eeb9b2c27cf2
Richard Grenon <richard@leankr.com>
2020-02-20 10:33:38 +0100

    async catching

--- api.js
+++ api.js
@@ -8,22 +8,28 @@ const service = require('restana')({
 const swagger = require('swagger-tools')
 const spec = require('./swagger.json')
 
+const router = service.newRouter()
+router.get('/nested-sync', (req, res) => {
+  throw new Error('Nest Sync Handler error catched ?')
+})
+router.get('/nested-async', async (req, res) => {
+  throw new Error('Nest Async Handler error catched ?')
+})
+
 swagger.initializeMiddleware(spec, async (middleware) => {
   service.use(middleware.swaggerMetadata())
   service.use(middleware.swaggerValidator())
   service.use(middleware.swaggerUi())
-  service.use(middleware.swaggerRouter({
-    controllers: path.join(__dirname, '/controllers')
-  }))
 
-  service.use(async (req, res, next) => {
-    try {
-      await next()
-    } catch (err) {
-      console.log('upps, something just happened')
-      res.send(err)
-    }
+  service.get('/direct-sync', (req,res,next) => {
+    throw new Error('Direct Sync Handler error catched ?')
   })
+
+  service.get('/direct-async', async (req,res,next) => {
+    throw new Error('Direct Async Handler error catched ?')
+  })
+
+  service.use('/', router)
 
   await service.start(4242)
   console.log('API documentation is now running at http://localhost:4242/docs/')
